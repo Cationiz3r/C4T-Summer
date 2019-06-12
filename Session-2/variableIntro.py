414c2b9db91597709d45ff087b2c318cb26ebc06
Cationiz3r <mordecai65970@gmail.com>
2019-06-12 21:15:06 +0700

    21:14

--- Session-2/variableIntro.py
+++ Session-2/variableIntro.py
@@ -4,7 +4,16 @@
 #a = input("Type a: ")
 #print(a)
 
+'''
 print()
 birth = int(input("Enter your year of birth: "))
 print("You're currently: ", 2019 -birth, " years old")
+print()
+'''
+
+print()
+height = float(input("  Input height h= "))
+width = float(input("  Input width  w= "))
+print()
+print("  The area of the triangle is: ", height *width /2,)
 print()
