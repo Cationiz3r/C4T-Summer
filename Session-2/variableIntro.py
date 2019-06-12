5d28debafc752c44c4f975d73693bbcf69c7a2f2
Cationiz3r <mordecai65970@gmail.com>
2019-06-12 20:26:57 +0700

    Jun 12

---
+++ Session-2/variableIntro.py
@@ -0,0 +1,10 @@
+#print("Hello me, myself and I")
+#print(1**6 - 2**6 + 3**6)
+
+#a = input("Type a: ")
+#print(a)
+
+print()
+birth = int(input("Enter your year of birth: "))
+print("You're currently: ", 2019 -birth, " years old")
+print()
